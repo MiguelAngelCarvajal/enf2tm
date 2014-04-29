b6fc668351dce4239b22c47d47bba17babd1e158
enfasis <enfasis@enfasis-PC>
2014-04-29 12:30:31 -0500

    Primer commit  Se puso el metodo para imprimir "Proyecto original"

---
+++ test/src/test/Test.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Test 
+
+{
+
+	
+	public Test() 
+	
+	 {
+	
+	 }
+
+	
+	public static void main(String[] args) 
+	
+	 {
+	   System.out.println("Proyecto Original");
+
+	 }
+
+}
